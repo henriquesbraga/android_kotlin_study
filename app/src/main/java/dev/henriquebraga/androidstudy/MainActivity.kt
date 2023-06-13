97894ea89ee4e6343c60af4ce173eaf8156b39d4
Henrique Braga <henriqueprof@outlook.com>
2023-06-13 15:46:01 -0300

    feat: viewBinding feature

--- app/src/main/java/dev/henriquebraga/androidstudy/MainActivity.kt
+++ app/src/main/java/dev/henriquebraga/androidstudy/MainActivity.kt
@@ -2,10 +2,27 @@ package dev.henriquebraga.androidstudy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dev.henriquebraga.androidstudy.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    // variable to control the binding
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setupActionListeners()
     }
+
+    private fun setupActionListeners() {
+        binding.button1.setOnClickListener {
+            binding.textLabel1.text = "You've pressed the button!!"
+        }
+    }
+
+
+
+
 }
